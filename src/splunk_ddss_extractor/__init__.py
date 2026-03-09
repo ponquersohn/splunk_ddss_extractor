be103fb82971ce7beaf76c5a965fa98930da7129
Lech Lachowicz <lech.lachowicz@pepsico.com>
2026-03-09 19:39:11 +0100

    Release v0.4.0

--- src/splunk_ddss_extractor/__init__.py
+++ src/splunk_ddss_extractor/__init__.py
@@ -2,7 +2,7 @@
 Splunk DDSS Extractor - Extract events from Splunk journal archives to raw format
 """
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __author__ = "Lech Lachowicz"
 
 from .decoder import Event, JournalDecoder
